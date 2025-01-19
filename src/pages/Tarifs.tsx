25ffd472e29a3dfecc1f77f68b17fcb572069b0b
Hassane <bezzahassane@gmail.com>
2025-01-20 00:18:54 +0100

    change tarif page

--- src/pages/Tarifs.tsx
+++ src/pages/Tarifs.tsx
@@ -26,78 +26,50 @@ const Tarifs: React.FC = () => {
             <tr>
               <th>Prestation</th>
               <th>Tarif</th>
-              <th>Exemple</th>
               <th>Illustration</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <td>Intervention simple</td>
-              <td>35€/00h30min</td>
-              <td>Exemple : Réparation de bugs logiciels courants.</td>
+              <td>35€</td>
               <td><img src="https://i.imgur.com/2mPhMbD.jpeg" alt="Installation routeur" className="image-small" /></td>
             </tr>
             <tr>
               <td>Intervention intermédiaire</td>
-              <td>50€/1h</td>
-              <td>Exemple : Configuration d’un réseau local ou dépannage matériel.</td>
+              <td>50€</td>
               <td><img src="https://i.imgur.com/B1i4E4i.jpg" alt="Réseaux locaux" className="image-small" /></td>
             </tr>
             <tr>
               <td>Intervention complexe</td>
-              <td>100€/2h</td>
-              <td>Exemple : Optimisation de la performance réseau ou mise à jour de serveurs.</td>
+              <td>100€</td>
               <td><img src="https://i.imgur.com/Dk7cIgw.jpeg" alt="Optimisation Wi-Fi" className="image-small" /></td>
             </tr>
             <tr>
               <td>Assistance, Accompagnement et Formation</td>
-              <td>50€/1h</td>
-              <td>Exemple : Formation sur l'utilisation d'outils bureautiques ou accompagnement personnalisé.</td>
+              <td>30€/1h</td>
               <td><img src="https://i.imgur.com/dsR94dQ.jpg" alt="Maintenance et dépannage" className="image-small" /></td>
             </tr>
             <tr>
               <td>Évaluation, Audit et sécurisation du réseau</td>
               <td>50€/1h</td>
-              <td>Exemple : Audit de sécurité pour protéger vos données personnelles.</td>
               <td><img src="https://i.imgur.com/uybf3qD.png" alt="Sécurisation réseau" className="image-small" /></td>
             </tr>
             <tr>
               <td>Maintenance et réparation des pièces d'ordinateur</td>
               <td>Prix variable selon la panne</td>
-              <td>Exemple : Remplacement de disque dur, carte mère, ou autres pièces défectueuses.</td>
               <td><img src="https://i.imgur.com/EcvAEQf.jpeg" alt="Réparation matériel" className="image-small" /></td>
             </tr>
             <tr>
               <td>Entretien et nettoyage</td>
               <td>20€</td>
-              <td>Exemple : Nettoyage interne de l'ordinateur, ventilation, suppression des fichiers temporaires.</td>
               <td><img src="https://i.imgur.com/z17iKib.jpeg" alt="Nettoyage ordinateur" className="image-small" /></td>
             </tr>
           </tbody>
         </table>
       </div>
 
-      <div className="mt-12">
-        <h2 className="text-xl font-semibold text-center text-blue-700 mb-4">
-          Complément de Prestation
-        </h2>
-        <table className="table-auto mx-auto mb-8">
-          <thead>
-            <tr>
-              <th className="px-4 py-2">Prestation</th>
-              <th className="px-4 py-2">Tarif</th>
-              <th className="px-4 py-2">Durée</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="border px-4 py-2">Complément</td>
-              <td className="border px-4 py-2">20€</td>
-              <td className="border px-4 py-2">20 minutes</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      
 
       <div className="mt-12">
         <h2 className="text-xl font-semibold text-center text-blue-700 mb-4">
@@ -112,16 +84,19 @@ const Tarifs: React.FC = () => {
           </thead>
           <tbody>
             <tr>
+            <td className="border px-4 py-2">Jusqu'à 10 km</td>
               <td className="border px-4 py-2">Gratuit</td>
-              <td className="border px-4 py-2">Jusqu'à 10 km</td>
+              
             </tr>
             <tr>
+            <td className="border px-4 py-2">10 à 20 km</td>
               <td className="border px-4 py-2">10€</td>
-              <td className="border px-4 py-2">10 à 20 km</td>
+              
             </tr>
             <tr>
+            <td className="border px-4 py-2">20 km et plus</td>
               <td className="border px-4 py-2">20€</td>
-              <td className="border px-4 py-2">20 km et plus</td>
+             
             </tr>
           </tbody>
         </table>
