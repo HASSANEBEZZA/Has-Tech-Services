4808d9808f5ef53a71f8bd1a30ae1e440633fb3f
Hassane <bezzahassane@gmail.com>
2025-01-19 02:58:40 +0100

    change Tarifs page

--- server.js
+++ server.js
@@ -2,38 +2,42 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
+// Importer les routes pour le formulaire de contact
+const contactRoutes = require('./routes/contactRoutes'); // Assurez-vous que le chemin est correct
+
+// Charger les variables d'environnement depuis le fichier .env
 dotenv.config();
 
 const app = express();
 
-// Liste des origines autorisées
+// Configurer CORS pour accepter les requêtes des origines spécifiques
 const allowedOrigins = [
-  'https://has-tech-service.vercel.app',  // Ton frontend Vercel
-  'https://www.has-tech-services.fr'     // Ton domaine en production
+  'https://has-tech-service.vercel.app',  // Frontend sur Vercel
+  'https://www.has-tech-services.fr',     // Ton domaine de production
 ];
 
 const corsOptions = {
   origin: function (origin, callback) {
-    // Vérifie si l'origine est autorisée
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true); // Permet l'accès si l'origine est autorisée
+      callback(null, true); // Autoriser l'origine
     } else {
-      callback(new Error('CORS not allowed')); // Refuse les autres origines
+      callback(new Error('CORS not allowed')); // Refuser l'origine
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'], // En-têtes autorisés
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
+app.use(cors(corsOptions));  // Activer CORS avec les options personnalisées
+
 // Middleware
-app.use(express.json());
-app.use(cors(corsOptions)); // Applique les options CORS
+app.use(express.json()); // Pour analyser les requêtes JSON
 
 // Routes
-app.use('/api/services/send-email', contactRoutes);  // Assure-toi que la route est correcte
+app.use('/api/services', contactRoutes); // Routes pour le formulaire de contact
 
 // Démarrer le serveur
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
