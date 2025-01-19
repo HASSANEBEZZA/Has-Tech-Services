7bdb804edf144b8d7980fc74864ce55e9a547cc7
Hassane <bezzahassane@gmail.com>
2025-01-19 02:51:38 +0100

    change Tarifs page

--- server.js
+++ server.js
@@ -2,35 +2,35 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
-// Charger les variables d'environnement
 dotenv.config();
-
-// Importer les routes pour le formulaire de contact
-const contactRoutes = require('./routes/contactRoutes');
 
 const app = express();
 
-// Spécifier les origines autorisées
-const allowedOrigins = ['https://has-tech-service.vercel.app']; // Ajoute l'URL de ton frontend
+// Liste des origines autorisées
+const allowedOrigins = [
+  'https://has-tech-service.vercel.app',  // Ton frontend Vercel
+  'https://www.has-tech-services.fr'     // Ton domaine en production
+];
 
 const corsOptions = {
   origin: function (origin, callback) {
+    // Vérifie si l'origine est autorisée
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true); // Permet l'accès si l'origine est autorisée
     } else {
       callback(new Error('CORS not allowed')); // Refuse les autres origines
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Spécifie les méthodes HTTP autorisées
-  allowedHeaders: ['Content-Type', 'Authorization'], // Spécifie les en-têtes autorisés
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'], // En-têtes autorisés
 };
 
 // Middleware
-app.use(express.json()); // Pour analyser les requêtes JSON
-app.use(cors(corsOptions)); // Utiliser les options CORS configurées
+app.use(express.json());
+app.use(cors(corsOptions)); // Applique les options CORS
 
 // Routes
-app.use('/api/services/send-email', contactRoutes); // Routes pour le formulaire de contact
+app.use('/api/services/send-email', contactRoutes);  // Assure-toi que la route est correcte
 
 // Démarrer le serveur
 const PORT = process.env.PORT || 3002;
