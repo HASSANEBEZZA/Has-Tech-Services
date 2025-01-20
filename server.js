f177aa64b1b17c7d36c0be1f5adc13aac41893a6
Hassane <bezzahassane@gmail.com>
2025-01-20 01:01:52 +0100

    change server.js

--- server.js
+++ server.js
@@ -10,33 +10,12 @@ dotenv.config();
 
 const app = express();
 
-// Configurer CORS pour accepter les requêtes des origines spécifiques
-const allowedOrigins = process.env.NODE_ENV === 'production' ? [
-  'https://has-tech-service.vercel.app',  // Frontend sur Vercel
-  'https://www.has-tech-services.fr',     // Ton domaine de production
-] : ['http://localhost:3000'];             // Frontend en local (par exemple)
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true); // Autoriser l'origine
-    } else {
-      callback(new Error('CORS not allowed')); // Refuser l'origine
-    }
-  },
-  methods: ['GET', 'POST', 'OPTIONS'],  // Ajout de la méthode OPTIONS pour gérer la pré-vérification
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  preflightContinue: false, // Important pour permettre le traitement de la pré-vérification
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));  // Activer CORS avec les options personnalisées
-
 // Middleware
 app.use(express.json()); // Pour analyser les requêtes JSON
+app.use(cors()); // Activer les CORS
 
 // Routes
-app.use('/api/services', contactRoutes); // Routes pour le formulaire de contact
+app.use('/api/contact', contactRoutes); // Routes pour le formulaire de contact
 
 // Démarrer le serveur
 const PORT = process.env.PORT || 3002;
