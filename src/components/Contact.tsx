979df37f1e7be4ad039c03c223e46065282750f6
Hassane <bezzahassane@gmail.com>
2025-03-05 00:40:01 +0100

    ajout de mentionlegal et Politique de confidentialité

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { FaTwitter, FaFacebook, FaInstagram, FaLinkedin, FaTiktok, FaYoutube } from 'react-icons/fa'; 
+import { FaTwitter, FaFacebook, FaInstagram, FaLinkedin, FaTiktok, FaYoutube } from 'react-icons/fa';
 import '../components/SocialMedia.scss';
 
 const Contact: React.FC = () => {
@@ -29,167 +29,50 @@ const Contact: React.FC = () => {
       return;
     }
 
-    try {
-      // Utiliser la variable d'environnement pour l'URL de l'API
-      const response = await axios.post(
-        process.env.REACT_APP_API_URL || '', // URL provenant de la variable d'environnement
-        formData
-      );
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+    if (!apiUrl) {
+      setStatus('error');
+      setErrorMessage("L'URL de l'API n'est pas configurée. Vérifiez vos variables d'environnement.");
+      return;
+    }
 
+    try {
+      const response = await axios.post(`${apiUrl}/contact`, formData);
       if (response.status === 200) {
         setStatus('success');
         setFormData({ name: '', email: '', message: '', acceptPrivacy: false });
       }
     } catch (error: any) {
       setStatus('error');
-      setErrorMessage(
-        error.response?.data?.error || "Une erreur s'est produite lors de l'envoi de l'email."
-      );
+      setErrorMessage(error.response?.data?.error || "Une erreur s'est produite lors de l'envoi de l'email.");
     }
   };
 
   return (
-    <>
-      <section id="contact" className="contact-section py-20 bg-gray-100">
-        <div className="container mx-auto text-center px-6">
-          <h2 className="text-5xl font-extrabold text-blue-700 mb-8">Contactez-nous</h2>
-          <p className="text-xl text-gray-700 mb-10">
-            Besoin d'assistance ? Contactez-nous via le formulaire ou par email à {'contact@has-tech-services.fr'}
-            <a href={`mailto:${process.env.REACT_APP_SUPPORT_EMAIL}`} className="text-blue-500 underline">
-              {process.env.REACT_APP_SUPPORT_EMAIL}
-            </a>
-            .
-          </p>
-          <form
-            className="bg-white p-12 shadow-lg rounded-lg max-w-5xl mx-auto space-y-8"
-            onSubmit={handleSubmit}
-          >
-            <div>
-              <input
-                type="text"
-                name="name"
-                placeholder="Votre nom"
-                className="w-full px-6 py-4 border border-gray-300 rounded-lg focus:ring focus:ring-blue-300 text-xl"
-                value={formData.name}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <input
-                type="email"
-                name="email"
-                placeholder="Votre email"
-                className="w-full px-6 py-4 border border-gray-300 rounded-lg focus:ring focus:ring-blue-300 text-xl"
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <textarea
-                name="message"
-                placeholder="Votre message"
-                className="w-full px-6 py-4 border border-gray-300 rounded-lg focus:ring focus:ring-blue-300 text-xl"
-                rows={6}
-                value={formData.message}
-                onChange={handleChange}
-                required
-              ></textarea>
-            </div>
-
-            {/* Case à cocher pour accepter la politique de confidentialité */}
-            <div>
-              <label className="flex items-center text-gray-700">
-                <input
-                  type="checkbox"
-                  name="acceptPrivacy"
-                  checked={formData.acceptPrivacy}
-                  onChange={handleCheckboxChange}
-                  className="mr-2"
-                />
-                J'accepte la <a href="/privacy-policy" className="text-blue-500">politique de confidentialité</a>.
-              </label>
-              {privacyError && <p className="text-red-600 text-sm">{privacyError}</p>}
-            </div>
-
-            <button
-              type="submit"
-              className="w-full py-3 bg-blue-700 text-white font-medium rounded-lg hover:bg-blue-600 text-xl"
-              disabled={status === 'loading'}
-            >
-              {status === 'loading' ? 'Envoi en cours...' : 'Envoyer'}
-            </button>
-          </form>
-
-          {status === 'success' && (
-            <p className="text-green-600 text-lg mt-4">Votre message a été envoyé avec succès !</p>
-          )}
-          {status === 'error' && (
-            <p className="text-red-600 text-lg mt-4">{errorMessage}</p>
-          )}
-        </div>
-      </section>
-
-      {/* Section Réseaux Sociaux */}
-      <section className="social-media-section py-6">
-        <div className="container mx-auto text-center max-w-4xl px-4">
-          <h3 className="text-lg font-bold text-white mb-4">
-            SUIVEZ-NOUS SUR LES <span className="font-extrabold">RÉSEAUX SOCIAUX</span>
-          </h3>
-          <div className="flex justify-center space-x-6">
-            <a
-              href="https://x.com/has_svc"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white text-2xl hover:text-gray-300"
-            >
-              <FaTwitter />
-            </a>
-            <a
-              href="https://www.facebook.com/people/Has-Tech-Services/61571620113575/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white text-2xl hover:text-gray-300"
-            >
-              <FaFacebook />
-            </a>
-            <a
-              href="https://www.instagram.com/has.tech.svc/?hl=en"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white text-2xl hover:text-gray-300"
-            >
-              <FaInstagram />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/has-tech-svc-10bb27345/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white text-2xl hover:text-gray-300"
-            >
-              <FaLinkedin />
-            </a>
-            <a
-              href="https://www.tiktok.com/@has.tech.svc"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white text-2xl hover:text-gray-300"
-            >
-              <FaTiktok />
-            </a>
-            <a
-              href="https://www.youtube.com/@HasTechServices"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white text-2xl hover:text-gray-300"
-            >
-              <FaYoutube />
-            </a>
-          </div>
-        </div>
-      </section>
-    </>
+    <section id="contact" className="contact-section py-20 bg-gray-100">
+      <div className="container mx-auto text-center px-6">
+        <h2 className="text-5xl font-extrabold text-blue-700 mb-8">Contactez-nous</h2>
+        <p className="text-xl text-gray-700 mb-10">
+          Besoin d'assistance ? Contactez-nous par email à
+          <a href="mailto:contact@has-tech-services.fr" className="text-blue-500 underline"> contact@has-tech-services.fr</a>.
+        </p>
+        <form className="bg-white p-12 shadow-lg rounded-lg max-w-5xl mx-auto space-y-8" onSubmit={handleSubmit}>
+          <input type="text" name="name" placeholder="Votre nom" className="input-field" value={formData.name} onChange={handleChange} required />
+          <input type="email" name="email" placeholder="Votre email" className="input-field" value={formData.email} onChange={handleChange} required />
+          <textarea name="message" placeholder="Votre message" className="input-field" rows={6} value={formData.message} onChange={handleChange} required></textarea>
+          <label className="flex items-center text-gray-700">
+            <input type="checkbox" name="acceptPrivacy" checked={formData.acceptPrivacy} onChange={handleCheckboxChange} className="mr-2" />
+            J'accepte la <a href="/privacy-policy" className="text-blue-500">politique de confidentialité</a>.
+          </label>
+          {privacyError && <p className="text-red-600 text-sm">{privacyError}</p>}
+          <button type="submit" className={`submit-btn ${!formData.acceptPrivacy ? 'disabled' : ''}`} disabled={!formData.acceptPrivacy || status === 'loading'}>
+            {status === 'loading' ? 'Envoi en cours...' : 'Envoyer'}
+          </button>
+        </form>
+        {status === 'success' && <p className="text-green-600 text-lg mt-4">Votre message a été envoyé avec succès !</p>}
+        {status === 'error' && <p className="text-red-600 text-lg mt-4">{errorMessage}</p>}
+      </div>
+    </section>
   );
 };
 
